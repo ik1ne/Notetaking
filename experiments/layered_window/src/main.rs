04ccd306c0c704d76eb978c7b748d5fa6c82e406
ik1ne <ik1ne@naver.com>
2025-04-29 12:48:02 +0900

    wip

--- experiments/layered_window/src/main.rs
+++ experiments/layered_window/src/main.rs
@@ -1,52 +1,40 @@
-use anyhow::Result;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::window::{Window, WindowId};
+use wry::{ScrollBarStyle, WebViewBuilder, WebViewBuilderExtWindows};
 
 #[derive(Default)]
 struct App {
     window: Option<Window>,
+    webview: Option<wry::WebView>,
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(
-            event_loop
-                .create_window(Window::default_attributes())
-                .unwrap(),
-        );
+        let window = event_loop
+            .create_window(Window::default_attributes())
+            .unwrap();
+        let webview = WebViewBuilder::new()
+            .with_html("hello world")
+            .build(&window)
+            .unwrap();
+
+        self.window = Some(window);
+        self.webview = Some(webview);
     }
 
     fn window_event(
         &mut self,
-        event_loop: &ActiveEventLoop,
-        window_id: WindowId,
+        _event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
         event: WindowEvent,
     ) {
-        match event {
-            WindowEvent::CloseRequested => {
-                println!("Close requested");
-                event_loop.exit();
-            }
-            WindowEvent::RedrawRequested => {
-                println!("Redraw requested");
-                self.window.as_mut().unwrap().request_redraw();
-            }
-            _ => {}
-        }
     }
 }
 
-fn main() -> Result<()> {
-    let event_loop = EventLoop::new()?;
-    event_loop.set_control_flow(ControlFlow::Poll);
-
-    event_loop.set_control_flow(ControlFlow::Wait);
-
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
     let mut app = App::default();
-
-    event_loop.run_app(&mut app)?;
-
-    Ok(())
+    event_loop.run_app(&mut app).unwrap();
 }
