39a4b60b4d6bb0c3d8078c60ac9edabaa75d2515
ik1ne <ik1ne@naver.com>
2025-04-29 12:15:57 +0900

    winit default example

--- experiments/layered_window/src/main.rs
+++ experiments/layered_window/src/main.rs
@@ -9,7 +9,44 @@ struct App {
     window: Option<Window>,
 }
 
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("Close requested");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                println!("Redraw requested");
+                self.window.as_mut().unwrap().request_redraw();
+            }
+            _ => {}
+        }
+    }
+}
+
 fn main() -> Result<()> {
     let event_loop = EventLoop::new()?;
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+
+    event_loop.run_app(&mut app)?;
+
     Ok(())
 }
