65994a5b95e4c05b250e4e1f74d001f593c794f1
ik1ne <ik1ne@naver.com>
2025-05-01 18:17:58 +0900

    wip

--- experiments/layered_window_cpp/main.cpp
+++ experiments/layered_window_cpp/main.cpp
@@ -1,17 +1,42 @@
 #define UNICODE
 #include <windows.h>
+#include <windowsx.h>
 #include <wrl.h>
 #include <WebView2.h>
+#include <iostream>
 
 #pragma comment(lib, "WebView2Loader.lib")
 
 using namespace Microsoft::WRL;
 
-// Global pointers
+// Globals
 ComPtr<ICoreWebView2Controller> g_controller;
 ComPtr<ICoreWebView2> g_webView;
+ComPtr<ICoreWebView2Environment3> g_env3;
+ComPtr<ICoreWebView2CompositionController> g_compController;
+HWND g_overlay = nullptr;
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+// Forward declarations
+LRESULT CALLBACK MainWndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK OverlayWndProc(HWND, UINT, WPARAM, LPARAM);
+
+// Minimal HTML content
+static const wchar_t* kHtml = LR"(
+<!DOCTYPE html>
+<html><body>
+  <h1>Hello World</h1>
+  <p>Overlay covers center half; pointer events logged.</p>
+  <p>Overlay covers center half; pointer events logged.</p>
+  <p>Overlay covers center half; pointer events logged.</p>
+  <p>Overlay covers center half; pointer events logged.</p>
+  <p>Overlay covers center half; pointer events logged.</p>
+  <p>Overlay covers center half; pointer events logged.</p>
+  <p>Overlay covers center half; pointer events logged.</p>
+</body></html>
+)";
+
+// Main window procedure: resize WebView and overlay
+LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 {
     switch (msg)
     {
@@ -19,10 +44,23 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
         {
             if (g_controller)
             {
-                RECT bounds;
-                GetClientRect(hwnd, &bounds);
-                g_controller->put_Bounds(bounds);
+                RECT rc;
+                GetClientRect(hwnd, &rc);
+                g_controller->put_Bounds(rc);
             }
+            // reposition overlay
+            RECT mrc;
+            GetClientRect(hwnd, &mrc);
+            POINT p = {mrc.left, mrc.top};
+            ClientToScreen(hwnd, &p);
+            int w = mrc.right - mrc.left;
+            int h = mrc.bottom - mrc.top;
+            MoveWindow(
+                g_overlay,
+                p.x + w / 4, p.y + h / 4,
+                w / 2, h / 2,
+                TRUE
+            );
             break;
         }
     case WM_DESTROY:
@@ -32,83 +70,140 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
     return DefWindowProcW(hwnd, msg, wp, lp);
 }
 
+// Overlay window procedure: paint black, log + forward pointer events
+LRESULT CALLBACK OverlayWndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch (msg)
+    {
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC dc = BeginPaint(hwnd, &ps);
+            RECT rc;
+            GetClientRect(hwnd, &rc);
+            HBRUSH brush = (HBRUSH)GetStockObject(BLACK_BRUSH);
+            FillRect(dc, &rc, brush);
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+    case WM_POINTERDOWN:
+    case WM_POINTERUPDATE:
+    case WM_POINTERUP:
+        {
+            UINT32 pid = GET_POINTERID_WPARAM(wp);
+            POINTER_INPUT_TYPE type;
+            if (GetPointerType(pid, &type))
+            {
+                const wchar_t* kind = L"Unknown";
+                if (type == PT_PEN) kind = L"Stylus";
+                else if (type == PT_MOUSE) kind = L"Mouse";
+                else if (type == PT_TOUCH) kind = L"Touch";
+                const wchar_t* phase =
+                    (msg == WM_POINTERDOWN) ? L"Down" : (msg == WM_POINTERUP) ? L"Up" : L"Move";
+                std::wcout << L"[" << kind << L"] " << phase << L" event" << std::endl;
+                std::wcout.flush();
+
+                if (type != PT_PEN && g_env3 && g_compController)
+                {
+                    ComPtr<ICoreWebView2PointerInfo> info;
+                    if (SUCCEEDED(g_env3->CreateCoreWebView2PointerInfo(&info)))
+                    {
+                        POINTER_INFO pi;
+                        if (GetPointerInfo(pid, &pi))
+                        {
+                            info->put_PointerKind(type);
+                            info->put_PointerId(pid);
+                            info->put_PointerFlags(pi.pointerFlags);
+                            info->put_PixelLocation(pi.ptPixelLocation);
+                            g_compController->SendPointerInput(
+                                (COREWEBVIEW2_POINTER_EVENT_KIND)msg,
+                                info.Get()
+                            );
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+    return DefWindowProcW(hwnd, msg, wp, lp);
+}
+
 int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE, PWSTR, int nCmdShow)
 {
     CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+    EnableMouseInPointer(TRUE);
 
     // Register main window
-    const wchar_t MAIN_CLS[] = L"WebView2Main";
+    const wchar_t MAIN_CLS[] = L"MainWebWnd";
     WNDCLASSW wc = {};
-    wc.lpfnWndProc = WndProc;
+    wc.lpfnWndProc = MainWndProc;
     wc.hInstance = hInst;
     wc.lpszClassName = MAIN_CLS;
     RegisterClassW(&wc);
 
-    // Register overlay window class
-    const wchar_t OVER_CLS[] = L"Overlay";
+    // Register overlay window (black background)
+    const wchar_t OVER_CLS[] = L"HalfOverlay";
     WNDCLASSW wc2 = {};
-    wc2.lpfnWndProc = DefWindowProcW;
+    wc2.lpfnWndProc = OverlayWndProc;
     wc2.hInstance = hInst;
     wc2.lpszClassName = OVER_CLS;
-    wc2.hbrBackground = CreateSolidBrush(RGB(0, 0, 0)); // black background
+    wc2.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wc2.style = CS_HREDRAW | CS_VREDRAW;
+    wc2.hCursor = LoadCursor(nullptr, IDC_ARROW);
     RegisterClassW(&wc2);
 
     // Create main window
     HWND hwnd = CreateWindowExW(
-        0, MAIN_CLS, L"WebView2 with Half Overlay",
+        0, MAIN_CLS, L"WebView2 + Half Overlay w/ Injection",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
         nullptr, nullptr, hInst, nullptr);
     ShowWindow(hwnd, nCmdShow);
 
-    // Create semi-transparent half-size overlay
+    // Create semi-transparent overlay covering center half
     RECT rc;
     GetClientRect(hwnd, &rc);
     int w = rc.right - rc.left;
     int h = rc.bottom - rc.top;
-    HWND overlay = CreateWindowExW(
+    POINT tl = {rc.left + w / 4, rc.top + h / 4};
+    ClientToScreen(hwnd, &tl);
+    g_overlay = CreateWindowExW(
         WS_EX_LAYERED | WS_EX_NOACTIVATE,
         OVER_CLS, nullptr,
         WS_POPUP | WS_VISIBLE,
-        rc.left + w / 4, rc.top + h / 4, // centered quarter offset
+        tl.x, tl.y,
         w / 2, h / 2,
         hwnd, nullptr, hInst, nullptr);
-    // 50% opacity black overlay
-    SetLayeredWindowAttributes(overlay, 0, 128, LWA_ALPHA);
-    SetWindowPos(overlay, HWND_TOPMOST, 0, 0, 0, 0,
+    SetLayeredWindowAttributes(g_overlay, 0, 192, LWA_ALPHA);
+    SetWindowPos(g_overlay, HWND_TOPMOST, 0, 0, 0, 0,
                  SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+    RegisterTouchWindow(g_overlay, 0);
 
-    // HTML content
-    const wchar_t* html = LR"(
-        <!DOCTYPE html>
-        <html><body>
-          <h1>Hello World</h1>
-          <p>Overlay covers center half.</p>
-        </body></html>
-    )";
-
-    // Initialize WebView2
+    // Initialize WebView2 and load HTML
     CreateCoreWebView2EnvironmentWithOptions(
         nullptr, nullptr, nullptr,
         Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
-            [hwnd, html](HRESULT envRes, ICoreWebView2Environment* env) -> HRESULT
+            [hwnd](HRESULT envRes, ICoreWebView2Environment* env) -> HRESULT
             {
                 if (SUCCEEDED(envRes) && env)
                 {
+                    env->QueryInterface(IID_PPV_ARGS(&g_env3));
                     env->CreateCoreWebView2Controller(
                         hwnd,
                         Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
-                            [hwnd, html](HRESULT ctrlRes, ICoreWebView2Controller* controller) -> HRESULT
+                            [hwnd](HRESULT ctrlRes, ICoreWebView2Controller* controller) -> HRESULT
                             {
                                 if (SUCCEEDED(ctrlRes) && controller)
                                 {
+                                    controller->QueryInterface(IID_PPV_ARGS(&g_compController));
                                     g_controller = controller;
                                     controller->get_CoreWebView2(&g_webView);
                                     controller->put_IsVisible(TRUE);
                                     RECT b;
                                     GetClientRect(hwnd, &b);
                                     controller->put_Bounds(b);
-                                    g_webView->NavigateToString(html);
+                                    g_webView->NavigateToString(kHtml);
                                 }
                                 return S_OK;
                             }).Get());
