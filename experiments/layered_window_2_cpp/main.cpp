9c139a48c5d6cdda497b65b55d0e1e85e9ca5512
ik1ne <ik1ne@naver.com>
2025-05-02 10:39:50 +0900

    wip

--- experiments/layered_window_2_cpp/main.cpp
+++ experiments/layered_window_2_cpp/main.cpp
@@ -1,14 +1,7 @@
 #include <windows.h>
-#include <wrl.h>
-#include <wil/com.h>
-#include <WebView2.h>
 
-using namespace Microsoft::WRL;
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-        _In_opt_ HINSTANCE hPrevInstance,
-        _In_ LPWSTR    lpCmdLine,
-        _In_ int       nCmdShow) {
-MessageBoxW(NULL, L"OverlayWebView2 App Started", L"Info", MB_OK);
-return 0;
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+    MessageBoxW(NULL, L"OverlayWebView2 App Started", L"Info", MB_OK);
+    return 0;
 }
