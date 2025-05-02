99c33dfc8a22b596d43a1027cc28765fafdb9449
ik1ne <ik1ne@naver.com>
2025-05-02 21:39:07 +0900

    wip

--- experiments/layered_window_2_cpp/main.cpp
+++ experiments/layered_window_2_cpp/main.cpp
@@ -3,7 +3,7 @@
 #include <windowsx.h>
 #include <wrl.h>
 #include <WebView2.h>
-#include <shellscalingapi.h>  // for DPI awareness
+#include <shellscalingapi.h>
 #include <vector>
 #include <mutex>
 #include <stdio.h>
@@ -19,7 +19,7 @@ static ComPtr<ICoreWebView2> g_webview;
 static std::vector<POINT> g_strokePoints;
 static std::mutex g_mutex;
 
-// Initialize WebView2
+// Create and size WebView2 inside main window
 void InitializeWebView(HWND hwnd)
 {
     CreateCoreWebView2EnvironmentWithOptions(
@@ -53,28 +53,27 @@ void InitializeWebView(HWND hwnd)
             }).Get());
 }
 
-// Resize WebView2 and overlay
+// Reposition/rescale both WebView2 control and overlay child
 void ResizeChildren(const RECT& rc)
 {
     if (g_controller)
     {
         g_controller->put_Bounds(rc);
     }
-    if (g_overlayHwnd && g_mainHwnd)
+    if (g_overlayHwnd)
     {
-        POINT pt{rc.left, rc.top};
-        ClientToScreen(g_mainHwnd, &pt);
+        // As a child window, position at 0,0 of parent client
         SetWindowPos(
             g_overlayHwnd,
-            HWND_TOPMOST,
-            pt.x, pt.y,
+            HWND_TOP,
+            0, 0,
             rc.right - rc.left,
             rc.bottom - rc.top,
             SWP_NOACTIVATE);
     }
 }
 
-// Main window procedure
+// Main window: forward size/move to children
 LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
@@ -89,90 +88,92 @@ LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         }
     case WM_DESTROY:
         PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProcW(hwnd, msg, wParam, lParam);
+        return 0;
     }
-    return 0;
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
 
-// Overlay window procedure: pen & mouse drawing
+// Overlay child window: draw strokes only
 LRESULT CALLBACK OverlayWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    if (msg == WM_POINTERDOWN || msg == WM_POINTERUPDATE || msg == WM_POINTERUP)
+    switch (msg)
     {
-        UINT32 pid = GET_POINTERID_WPARAM(wParam);
-        POINTER_INPUT_TYPE type;
-        if (SUCCEEDED(GetPointerType(pid, &type)) && type == PT_PEN)
+    case WM_POINTERDOWN:
+    case WM_POINTERUPDATE:
+    case WM_POINTERUP:
         {
-            POINTER_INFO pi;
-            if (GetPointerInfo(pid, &pi))
+            UINT32 pid = GET_POINTERID_WPARAM(wParam);
+            POINTER_INPUT_TYPE type;
+            if (SUCCEEDED(GetPointerType(pid, &type)) && type == PT_PEN)
             {
-                POINT pt = pi.ptPixelLocation;
-                ScreenToClient(hwnd, &pt);
-                std::lock_guard<std::mutex> lock(g_mutex);
-                if (msg == WM_POINTERDOWN)
+                POINTER_INFO pi;
+                if (GetPointerInfo(pid, &pi))
                 {
-                    g_strokePoints.clear();
-                    g_strokePoints.push_back(pt);
-                    SetCapture(hwnd);
-                }
-                else if (msg == WM_POINTERUPDATE)
-                {
-                    g_strokePoints.push_back(pt);
-                    InvalidateRect(hwnd, nullptr, FALSE);
-                }
-                else if (msg == WM_POINTERUP)
-                {
-                    ReleaseCapture();
+                    POINT pt = pi.ptPixelLocation;
+                    ScreenToClient(hwnd, &pt);
+                    std::lock_guard<std::mutex> lock(g_mutex);
+                    if (msg == WM_POINTERDOWN)
+                    {
+                        g_strokePoints.clear();
+                        g_strokePoints.push_back(pt);
+                        SetCapture(hwnd);
+                    }
+                    else if (msg == WM_POINTERUPDATE)
+                    {
+                        g_strokePoints.push_back(pt);
+                        InvalidateRect(hwnd, nullptr, FALSE);
+                    }
+                    else
+                    {
+                        ReleaseCapture();
+                    }
                 }
             }
+            return 0;
         }
-        return 0;
-    }
-    // Fallback to mouse events
-    if (msg == WM_LBUTTONDOWN || msg == WM_MOUSEMOVE || msg == WM_LBUTTONUP)
-    {
-        bool btnDown = (wParam & MK_LBUTTON) != 0;
-        POINT pt{GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)};
-        std::lock_guard<std::mutex> lock(g_mutex);
-        if (msg == WM_LBUTTONDOWN)
+    case WM_LBUTTONDOWN:
+    case WM_MOUSEMOVE:
+    case WM_LBUTTONUP:
         {
-            g_strokePoints.clear();
-            g_strokePoints.push_back(pt);
-            SetCapture(hwnd);
+            bool down = (wParam & MK_LBUTTON) != 0;
+            POINT pt{GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)};
+            std::lock_guard<std::mutex> lock(g_mutex);
+            if (msg == WM_LBUTTONDOWN)
+            {
+                g_strokePoints.clear();
+                g_strokePoints.push_back(pt);
+                SetCapture(hwnd);
+            }
+            else if (msg == WM_MOUSEMOVE && down && GetCapture() == hwnd)
+            {
+                g_strokePoints.push_back(pt);
+                InvalidateRect(hwnd, nullptr, FALSE);
+            }
+            else if (msg == WM_LBUTTONUP)
+            {
+                ReleaseCapture();
+            }
+            return 0;
         }
-        else if (msg == WM_MOUSEMOVE && btnDown && GetCapture() == hwnd)
+    case WM_PAINT:
         {
-            g_strokePoints.push_back(pt);
-            InvalidateRect(hwnd, nullptr, FALSE);
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            HPEN pen = CreatePen(PS_SOLID, 3, RGB(255, 0, 0));
+            HPEN old = (HPEN)SelectObject(hdc, pen);
+            std::lock_guard<std::mutex> lock(g_mutex);
+            for (size_t i = 1; i < g_strokePoints.size(); ++i)
+            {
+                MoveToEx(hdc, g_strokePoints[i - 1].x, g_strokePoints[i - 1].y, nullptr);
+                LineTo(hdc, g_strokePoints[i].x, g_strokePoints[i].y);
+            }
+            SelectObject(hdc, old);
+            DeleteObject(pen);
+            EndPaint(hwnd, &ps);
+            return 0;
         }
-        else if (msg == WM_LBUTTONUP)
-        {
-            ReleaseCapture();
-        }
-        return 0;
-    }
-    if (msg == WM_PAINT)
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-        HPEN pen = CreatePen(PS_SOLID, 3, RGB(255, 0, 0));
-        HPEN old = (HPEN)SelectObject(hdc, pen);
-        std::lock_guard<std::mutex> lock(g_mutex);
-        for (size_t i = 1; i < g_strokePoints.size(); ++i)
-        {
-            MoveToEx(hdc, g_strokePoints[i - 1].x, g_strokePoints[i - 1].y, nullptr);
-            LineTo(hdc, g_strokePoints[i].x, g_strokePoints[i].y);
-        }
-        SelectObject(hdc, old);
-        DeleteObject(pen);
-        EndPaint(hwnd, &ps);
-        return 0;
-    }
-    if (msg == WM_ERASEBKGND)
-    {
-        return 1;
+    case WM_ERASEBKGND:
+        return 1; // no background erase
     }
     return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
@@ -180,7 +181,7 @@ LRESULT CALLBACK OverlayWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPara
 // Entry point
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 {
-    // Enable per-monitor DPI awareness for correct coordinate mapping
+    // DPI awareness
     SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
     CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
     EnableMouseInPointer(TRUE);
@@ -194,44 +195,54 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     RegisterClassW(&wc);
 
-    // Register overlay window
+    // Register overlay as child with transparent background
     WNDCLASSW wc2 = {};
     wc2.lpfnWndProc = OverlayWndProc;
     wc2.hInstance = hInstance;
     wc2.lpszClassName = L"OverlayWindowClass";
     wc2.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wc2.hbrBackground = nullptr;
+    wc2.style = CS_HREDRAW | CS_VREDRAW;
     RegisterClassW(&wc2);
 
     // Create main window
     g_mainHwnd = CreateWindowExW(
-        0,
-        L"MainWindowClass",
-        L"Two-Layer Note-Taking",
+        0, L"MainWindowClass", L"Two-Layer Note-Taking",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT,
         1024, 768,
         nullptr, nullptr, hInstance, nullptr);
     ShowWindow(g_mainHwnd, nCmdShow);
 
-    // Initialize WebView2
+    // Setup WebView2
     InitializeWebView(g_mainHwnd);
 
-    // Create overlay
-    g_overlayHwnd = CreateWindowExW(
-        WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_TOOLWINDOW | WS_EX_NOACTIVATE,
-        L"OverlayWindowClass",
-        nullptr, WS_POPUP,
-        0, 0, 0, 0,
-        nullptr, nullptr, hInstance, nullptr);
-    SetLayeredWindowAttributes(g_overlayHwnd, 0, 128, LWA_ALPHA);
-    ShowWindow(g_overlayHwnd, nCmdShow);
-
-    // Initial sizing
+    // Get the client rectangle for correct overlay dimensions
     RECT rc;
     GetClientRect(g_mainHwnd, &rc);
+
+    // Create overlay as a layered top-level window to show strokes over WebView2
+    g_overlayHwnd = CreateWindowExW(
+        WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_TOOLWINDOW | WS_EX_NOACTIVATE,
+        L"OverlayWindowClass", nullptr,
+        WS_POPUP,
+        0, 0,
+        rc.right - rc.left,
+        rc.bottom - rc.top,
+        nullptr, // no parent
+        nullptr, // no menu
+        hInstance,
+        nullptr // no additional params
+    );
+    // Make black transparent so only strokes show
+    SetLayeredWindowAttributes(g_overlayHwnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
+    ShowWindow(g_overlayHwnd, nCmdShow);
+
+    // Overlay initially placed and sized; use ResizeChildren for future WM_SIZE/WM_MOVE
     ResizeChildren(rc);
 
-    // Main loop
+
+    // Run message loop
     MSG msg;
     while (GetMessage(&msg, nullptr, 0, 0))
     {
