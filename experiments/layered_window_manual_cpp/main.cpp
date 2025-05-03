a0023ffcd3edb415bbcd20d6da0e06da6be2640e
ik1ne <ik1ne@naver.com>
2025-05-03 20:36:39 +0900

    wip

---
+++ experiments/layered_window_manual_cpp/main.cpp
@@ -0,0 +1,16 @@
+#define UNICODE
+#include <windows.h>
+#include <windowsx.h>
+#include <wrl.h>
+#include <WebView2.h>
+#include <mutex>
+
+#pragma comment(lib, "Shcore.lib")
+#pragma comment(lib, "WebView2Loader.lib")
+
+using namespace Microsoft::WRL;
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
+{
+    return 0;
+}
